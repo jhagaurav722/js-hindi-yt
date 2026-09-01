976514c90192e138c0dde55b023715cab5d5be27
Gaurav Jha <jhagaurav722@gmail.com>
2026-09-01 08:32:38 +0000

    conersionOperations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = "33abc";
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueinNumber = Number(score);
+// console.log(typeof(valueinNumber))
+// console.log(valueinNumber)
+
+//"33"=> 33
+//"33abc"=>NaN
+//true=>1; false=>0
+
+let isLoggedIn = "";
+let booleanisLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanisLoggedIn);
+
+//1=>true; 0=>false;
+//""=>false;
+//"hitesh"=> true;
+
+let someNumber = 33;
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
