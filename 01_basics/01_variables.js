1f5bbcba43e0e58efb7c065cefb637a212e378d4
Gaurav Jha <jhagaurav722@gmail.com>
2026-09-01 07:35:30 +0000

    variables concept done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 12345;
+let accountEmail = "gaurav@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+
+// accountId = 123456;
+
+accountEmail = "gaurav123@gmail.com";
+accountPassword = "54321";
+accountCity = "Delhi";
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountState,accountCity])
